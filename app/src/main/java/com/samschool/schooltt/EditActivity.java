377511b1488e6e9e2deba96a84e0aa6a600c6696
lexa <lexabor@mail.ru>
2015-05-02 20:44:53 +0300

    - добавлены новые файлы;

---
+++ app/src/main/java/com/samschool/schooltt/EditActivity.java
@@ -0,0 +1,93 @@
+package com.samschool.schooltt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+
+import com.samschool.schooltt.pages.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class EditActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+    final String FILENAME = "file";
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tv = (TextView) findViewById(R.id.textView1);
+
+
+    }
+
+    public void onclick(View v) {
+        switch (v.getId()) {
+            case R.id.btnWrite:
+                writeFile();
+                break;
+            case R.id.btnRead:
+                readFile();
+                break;
+        }
+    }
+
+    void writeFile() {
+        try {
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                    openFileOutput(FILENAME, MODE_PRIVATE)));
+
+            bw.write("Содержимое файла");
+
+            bw.close();
+
+            Log.d(LOG_TAG, "Файл записан");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void readFile() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput(FILENAME)));
+            String str = "";
+
+            while ((str = br.readLine()) != null) {
+                Log.d(LOG_TAG, str);
+                tv.setText(str);
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
